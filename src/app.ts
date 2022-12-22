aaa72875c3daabbaaded211e4878024869f7afd9
Leandro Assis <leandrou9@gmail.com>
2022-12-22 10:38:33 -0300

    Constructor functions e funcionamento do this (Compass.Uol)

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 class Departamento {
     nome: string;
+    empregados: string[] = [];
 
     constructor(n: string) {
         this.nome = n;
     }
+
+    describe(this: Departamento) {
+        console.log('Departamento: ' + this.nome);
+    }
 }
 
 const contabilidade = new Departamento('Contabilidade');
-console.log(contabilidade);
+contabilidade.describe();
+
+const contabilidadeCopia = { nome: 'Qulauqer', describe: contabilidade.describe };
+contabilidadeCopia.describe();

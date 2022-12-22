b13613ae325e07742916f59e32f995508b0cb7f0
Leandro Assis <leandrou9@gmail.com>
2022-12-22 13:57:04 -0300

    Combinando Interfaces com extends (Compass.Uol)

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
-interface Greetable {
-    nome: string;
+interface Nomeado {
+    readonly nome: string;
+}
 
+//interfaces podem herdar mais de uma interface, classes herdam paenas uma classe
+interface Greetable extends Nomeado {
     greet(frase: string): void;
 }
 
-class Pessoa implements Greetable {
+class Pessoa implements Greetable, Nomeado {
     nome: string;
+    idade: number;
 
-    constructor(n: string) {
+    constructor(n: string, idade: number) {
         this.nome = n;
+        this.idade = idade;
     }
 
     greet(frase: string) {
@@ -18,7 +23,7 @@ class Pessoa implements Greetable {
 
 let user1: Greetable;
 
-user1 = new Pessoa('Alejandro');
+user1 = new Pessoa('Alejandro', 77);
 
 user1.greet('Hola, yo soy');
 console.log(user1);

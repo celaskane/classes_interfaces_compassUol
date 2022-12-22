ce712513e9350a5b97f328d4b282bedc2b4cebf4
Leandro Assis <leandrou9@gmail.com>
2022-12-22 13:44:43 -0300

    Usando Interfaces com Classes (Compass.Uol)

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
-interface Pessoa {
+interface Greetable {
     nome: string;
-    idade: number;
 
     greet(frase: string): void;
 }
 
-let user1: Pessoa;
-user1 = {
-    nome: 'Alejandro',
-    idade: 77,
-    greet(frase: string) {
-        console.log (frase + ' ' + this.nome);
+class Pessoa implements Greetable {
+    nome: string;
+
+    constructor(n: string) {
+        this.nome = n;
     }
-};
+
+    greet(frase: string) {
+        console.log(frase + ' ' + this.nome);
+    }
+}
+
+let user1: Greetable;
+
+user1 = new Pessoa('Alejandro');
 
 user1.greet('Hola, yo soy');
+console.log(user1);

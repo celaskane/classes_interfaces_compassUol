6e0f16b582fe4ca2409b4c7cb70b913d99fc38c8
Leandro Assis <leandrou9@gmail.com>
2022-12-22 10:46:08 -0300

    Modificadores de acesso public e private (Compass.Uol)

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 class Departamento {
-    nome: string;
-    empregados: string[] = [];
+    nome: string;   //public padrão (não precisa declarar)
+    private empregados: string[] = [];
 
     constructor(n: string) {
         this.nome = n;
@@ -9,10 +9,23 @@ class Departamento {
     describe(this: Departamento) {
         console.log('Departamento: ' + this.nome);
     }
+
+    addEmpregado(empregado: string) {
+        this.empregados.push(empregado);
+    }
+
+    mostraInformacaoEmpregado() {
+        console.log(this.empregados.length);
+        console.log(this.empregados);
+    }
 }
 
 const contabilidade = new Departamento('Contabilidade');
+contabilidade.addEmpregado('Alejandro');
+contabilidade.addEmpregado('Lisandro');
+/* contabilidade.empregados[2] = 'Leandros'; */   //acessível de fora da classe (adicoinar private)
 contabilidade.describe();
+contabilidade.mostraInformacaoEmpregado();
 
-const contabilidadeCopia = { nome: 'Qulauqer', describe: contabilidade.describe };
-contabilidadeCopia.describe();
+/* const contabilidadeCopia = { nome: 'Qulauqer', describe: contabilidade.describe };
+contabilidadeCopia.describe(); */
